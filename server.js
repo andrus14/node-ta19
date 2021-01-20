32c82ec2736a7fd305d3bbf2c145cdee62e0e054
Andrus Naulainen <andrus.naulainen@gmail.com>
2021-01-20 10:08:41 +0200

    express sessions and redis

--- server.js
+++ server.js
@@ -1,10 +1,80 @@
 const express = require('express');
 const app = express();
+
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+
+const session = require('express-session');
+const redis = require('redis');
+const connectRedis = require('connect-redis');
+
+var bodyParser = require('body-parser');
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '/public')));
+// config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// redis server
+const RedisStore = connectRedis(session);
+
+//Configure redis client
+const redisClient = redis.createClient({
+  host: 'localhost',
+  port: 6379
+})
+
+redisClient.on('error', function (err) {
+  console.log('Could not establish a connection with redis. ' + err);
+});
+redisClient.on('connect', function (err) {
+  console.log('Connected to redis successfully');
+});
+
+const sessionMiddleWare = session({
+  store: new RedisStore({ client: redisClient }),
+  secret: 'secret$%^134',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+      secure: false, // if true only transmit cookie over https
+      httpOnly: false, // if true prevent client side JS from reading the cookie 
+      maxAge: 1000 * 60 * 60 * 24 // session max age in miliseconds
+  }
+});
+
+app.use(sessionMiddleWare);
+
+app.get("/", (req, res) => {
+  sess = req.session;
+  if (sess.username && sess.password) {
+      sess = req.session;
+      if (sess.username) {
+        res.sendFile(__dirname + '/public/index.html');
+      }
+  } else {
+      res.sendFile(__dirname + "/public/login.html");
+  }
+});
+
+app.post("/login", (req, res) => {
+  sess = req.session;
+  sess.username = req.body.username
+  sess.password = req.body.password
+  // add username and password validation logic here if you want. If user is authenticated send the response as success
+  res.end("success")
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy(err => {
+      if (err) {
+          return console.log(err);
+      }
+      res.redirect("/")
+  });
+});
 
 io.on('connection', (socket) => {
     console.log(socket.id, 'a user connected');
@@ -13,8 +83,6 @@ io.on('connection', (socket) => {
         io.emit('chat_message', msg);
     });
 });
-
-
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
